b7ed07836f1ff95db9c5498e307f489e1679930f
pauliinatraining <105265510+pauliinatraining@users.noreply.github.com>
2022-05-10 09:11:02 +0300

    Update streamlit_app.py
    import pandas
    Read fruit macro from s3
    Print fruit macro

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
+//Display:
 
 st.title('Mom and Pop\'s New Healthy Diner')
 st.header('Breakfast Favorites')
@@ -8,3 +14,4 @@ st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.dataframe(my_fruit_list)
