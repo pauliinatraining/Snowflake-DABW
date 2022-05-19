dd33b237c67d386579d7d1e930b7cbd356de6084
pauliinatraining <105265510+pauliinatraining@users.noreply.github.com>
2022-05-19 12:10:39 +0300

    Update streamlit_app.py
    Add fruit input for fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,6 @@ import requests
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-current_fruit = "kiwi"
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + current_fruit)
-
 # Display:
 
 st.title('Mom and Pop\'s New Healthy Diner')
@@ -28,6 +25,10 @@ st.dataframe(fruits_to_show)
 
 # Display fruityvice api response
 st.header('Fruityvice Fruit Advice!')
+
+fruit_choice = st.text_input('What fruit would you like to learn about?', 'Kiwi')
+st.write('You entered', current_fruit)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + current_fruit)
 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 st.dataframe(fruityvice_normalized)
