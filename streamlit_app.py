e96be1f09d48efc64fb26f542e6aa358df78a763
pauliinatraining <105265510+pauliinatraining@users.noreply.github.com>
2022-05-19 12:07:57 +0300

    Update streamlit_app.py
    add fruit name to a variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ import requests
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+current_fruit = "kiwi"
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + current_fruit)
 
 # Display:
 
