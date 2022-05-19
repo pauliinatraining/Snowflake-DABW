cf48bb023e9136091b0bc5e83d733148231c43ec
pauliinatraining <105265510+pauliinatraining@users.noreply.github.com>
2022-05-19 12:01:38 +0300

    Update streamlit_app.py
    import requests
    add fruityvice_response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
+import requests
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 # Display:
 
@@ -22,3 +24,5 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #Display the fruits table
 st.dataframe(fruits_to_show)
+
+st.text(fruityvice_respose)
