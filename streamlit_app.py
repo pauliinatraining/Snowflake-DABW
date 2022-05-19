1d7e88e72bd1d882037ceebf577e20479b308bbe
pauliinatraining <105265510+pauliinatraining@users.noreply.github.com>
2022-05-19 14:02:36 +0300

    Update streamlit_app.py
    add try except for fruityvice information

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import requests
 import snowflake.connector
+from urllib.error import ERLError
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -26,14 +27,19 @@ st.dataframe(fruits_to_show)
 
 # Display fruityvice api response
 st.header('Fruityvice Fruit Advice!')
+try:
+  fruit_choice = st.text_input('What fruit would you like to learn about?')
+  if not fruit_choice:
+    st.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    st.dataframe(fruityvice_normalized)
+    
+except URLError as e:
+  st.error()
 
-fruit_choice = st.text_input('What fruit would you like to learn about?', 'Kiwi')
-st.write('You entered', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-st.dataframe(fruityvice_normalized)
-
+st.stop()
 #snowflake
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -46,3 +52,6 @@ st.dataframe(my_data_rows)
 # Allow end user to add a fruit to the list
 add_my_fruit = st.text_input('What fruit would you like to add?')
 st.write('Thanks for adding ' + add_my_fruit)
+
+#Add fruit to snowflake
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
